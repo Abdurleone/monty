54400c1d6b14f8a446ecbe6f934d97f86608ea24
Steph <stephanieamadi@gmail.com>
2023-08-25 01:22:35 +0300

    Tasks

--- addtask4.c
+++ addtask4.c
@@ -8,20 +8,20 @@
  */
 void f_add(stack_t **head, unsigned int counter)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    if (*head == NULL || (*head)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    temp = *head;
-    (*head)->next->n += (*head)->n;
-    *head = (*head)->next;
-    (*head)->prev = NULL;
-    free(temp);
+	temp = *head;
+	(*head)->next->n += (*head)->n;
+	*head = (*head)->next;
+	(*head)->prev = NULL;
+	free(temp);
 }
